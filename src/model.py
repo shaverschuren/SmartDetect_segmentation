445994aa59eb1c56e26adbc6b276c58de2cd1424
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-03-12 13:18:05 +0100

    Added an initial model architecture file.

---
+++ src/model.py
@@ -0,0 +1,161 @@
+from keras.optimizers import Adam
+from keras.initializers import RandomNormal
+from keras.models import Model
+from keras.models import Input
+from keras.layers import Conv2D
+from keras.layers import Conv2DTranspose
+from keras.layers import LeakyReLU
+from keras.layers import Activation
+from keras.layers import Concatenate
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
+
+
+def define_discriminator(image_shape=(256, 256, 1)):
+    """
+    This function defines the discriminator architecture.
+    It may vary the architecture as a function of passed input shapes.
+    """
+
+	# weight initialization
+    init = RandomNormal(stddev=0.02)
+	# source image input
+    in_src_image = Input(shape=image_shape)
+	# target image input
+    in_target_image = Input(shape=image_shape)
+	# concatenate images channel-wise
+    merged = Concatenate()([in_src_image, in_target_image])
+	# C64
+    d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+    d = LeakyReLU(alpha=0.2)(d)
+	# C128
+    d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+	# C256
+    d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+	# C512
+    d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+	# second last output layer
+    d = Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+	# patch output
+    d = Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
+    patch_out = Activation('sigmoid')(d)
+	# define model
+    model = Model([in_src_image, in_target_image], patch_out)
+	# compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, loss_weights=[0.5])
+
+    return model
+
+
+def define_encoder_block(layer_in, n_filters, batchnorm=True):
+    """
+    Here, we define the encoder block.
+    """
+
+	# weight initialization
+    init = RandomNormal(stddev=0.02)
+	# add downsampling layer
+    g = Conv2D(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+	# conditionally add batch normalization
+    if batchnorm:
+        g = BatchNormalization()(g, training=True)
+	# leaky relu activation
+    g = LeakyReLU(alpha=0.2)(g)
+
+    return g
+
+
+def decoder_block(layer_in, skip_in, n_filters, dropout=True):
+    """
+    Here, we define the decoder block.
+    """
+
+	# weight initialization
+    init = RandomNormal(stddev=0.02)
+	# add upsampling layer
+    g = Conv2DTranspose(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+	# add batch normalization
+    g = BatchNormalization()(g, training=True)
+	# conditionally add dropout
+    if dropout:
+        g = Dropout(0.5)(g, training=True)
+	# merge with skip connection
+    g = Concatenate()([g, skip_in])
+	# relu activation
+    g = Activation('relu')(g)
+
+    return g
+
+
+def define_generator(image_shape=(256, 256, 1)):
+    """
+    This funcion defines the generator model architecture.
+    It may vary the architecture as a function of the passed input shape.
+    """
+
+	# weight initialization
+    init = RandomNormal(stddev=0.02)
+	# image input
+    in_image = Input(shape=image_shape)
+	# encoder model
+    e1 = define_encoder_block(in_image, 64, batchnorm=False)
+    e2 = define_encoder_block(e1, 128)
+    e3 = define_encoder_block(e2, 256)
+    e4 = define_encoder_block(e3, 512)
+    e5 = define_encoder_block(e4, 512)
+    e6 = define_encoder_block(e5, 512)
+    e7 = define_encoder_block(e6, 512)
+	# bottleneck, no batch norm and relu
+    b = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(e7)
+    b = Activation('relu')(b)
+	# decoder model
+    d1 = decoder_block(b, e7, 512)
+    d2 = decoder_block(d1, e6, 512)
+    d3 = decoder_block(d2, e5, 512)
+    d4 = decoder_block(d3, e4, 512, dropout=False)
+    d5 = decoder_block(d4, e3, 256, dropout=False)
+    d6 = decoder_block(d5, e2, 128, dropout=False)
+    d7 = decoder_block(d6, e1, 64, dropout=False)
+    
+	# Changed the 1 below from 3 (gray ipv RGB)
+    g = Conv2DTranspose(1, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d7)
+    out_image = Activation('tanh')(g)
+	# define model
+    model = Model(in_image, out_image)
+
+    return model
+
+
+def define_gan(g_model, d_model, image_shape):
+    """
+    Here, we define the final gan model, based on the generator and discriminator models.
+    We also pass the input shape.
+    """
+
+	# make weights in the discriminator not trainable
+    for layer in d_model.layers:
+        if not isinstance(layer, BatchNormalization):
+            layer.trainable = False
+	# define the source image
+    in_src = Input(shape=image_shape)
+	# connect the source image to the generator input
+    gen_out = g_model(in_src)
+	# connect the source input and generator output to the discriminator input
+    dis_out = d_model([in_src, gen_out])
+	# src image as input, generated image and classification output
+    model = Model(in_src, [dis_out, gen_out])
+
+	# compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1,100])
+    
+    return model
