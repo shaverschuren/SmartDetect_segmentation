0283d591f2920bd57bf8eb0d89f8184341cb1ec3
HJD98 <51057163+HJD98@users.noreply.github.com>
2021-03-29 14:58:01 +0200

    call training function in notebook

--- src/training.py
+++ src/training.py
@@ -55,39 +55,45 @@ def generate_fake_samples(g_model, samples, patch_shape):
 	y = np.zeros((len(X), patch_shape, patch_shape, 1))
 	return X, y 
   
-# generate samples and save as a plot and save the model
 def summarize_performance(step, g_model, dataset, n_samples=3):
 	# select a sample of input images
-	[X_realA, X_realB], _ = generate_real_samples(dataset, n_samples, 1)
-	# generate a batch of fake samples
-	X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
+    [X_realA, X_realB], _ = generate_real_samples(dataset, n_samples, 1)
+	#X_realA = X_realA[:,:]
+    # generate a batch of fake samples
+    X_fakeB, _ = generate_fake_samples(g_model, X_realA, 1)
 	# scale all pixels from [-1,1] to [0,1]
-	X_realA = (X_realA + 1) / 2.0
-	X_realB = (X_realB + 1) / 2.0
-	X_fakeB = (X_fakeB + 1) / 2.0
-	# plot real source images
-	for i in range(n_samples):
-		plt.subplot(3, n_samples, 1 + i)
-		plt.axis('off')
-		plt.imshow(X_realA[i])
+    X_realA = ((X_realA + 1) / 2.0)[:,:,:,0]
+    X_realB = ((X_realB + 1) / 2.0)[:,:,:,0]
+    X_fakeB = ((X_fakeB + 1) / 2.0)[:,:,:,0]
+    # plot real source images
+    for i in range(n_samples):
+        plt.figure(figsize=(10,10))
+        plt.subplot(3, n_samples, 1 + i)
+        plt.axis('off')
+        plt.imshow(X_realA[i], aspect='equal')
+        plt.show()
 	# plot generated target image
-	for i in range(n_samples):
-		plt.subplot(3, n_samples, 1 + n_samples + i)
-		plt.axis('off')
-		plt.imshow(X_fakeB[i])
+    for i in range(n_samples):
+        plt.figure(figsize=(10,10))
+        plt.subplot(3, n_samples, 1 + n_samples + i)
+        plt.axis('off')
+        plt.imshow(X_fakeB[i], aspect='equal')
+        plt.show()
 	# plot real target image
-	for i in range(n_samples):
-		plt.subplot(3, n_samples, 1 + n_samples*2 + i)
-		plt.axis('off')
-		plt.imshow(X_realB[i])
+    for i in range(n_samples):
+        plt.figure(figsize=(10,10))
+        plt.subplot(3, n_samples, 1 + n_samples*2 + i)
+        plt.axis('off')
+        plt.imshow(X_realB[i], aspect='equal')
+        plt.show()
 	# save plot to file
-	filename1 = 'plot_%06d.png' % (step+1)
-	plt.savefig(filename1)
-	plt.close()
+    filename1 = 'plot_%06d.png' % (step+1)
+    plt.savefig(filename1)
+    plt.close()
 	# save the generator model
-	filename2 = 'model_%06d.h5' % (step+1)
-	g_model.save(filename2)
-	print('>Saved: %s and %s' % (filename1, filename2))  
+    filename2 = 'model_%06d.h5' % (step+1)
+    g_model.save(filename2)
+    print('>Saved: %s and %s' % (filename1, filename2))  
   
 	
 # train pix2pix model
