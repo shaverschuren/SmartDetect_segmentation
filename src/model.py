b3f417ad0ced0fd1532b412d28596d09365bc82c
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-05-06 20:04:23 +0200

    Style

--- src/model.py
+++ src/model.py
@@ -17,39 +17,39 @@ def define_discriminator(image_shape=(256, 256, 1)):
     It may vary the architecture as a function of passed input shapes.
     """
 
-	# weight initialization
+    # weight initialization
     init = RandomNormal(stddev=0.02)
-	# source image input
+    # source image input
     in_src_image = Input(shape=image_shape)
-	# target image input
+    # target image input
     in_target_image = Input(shape=image_shape)
-	# concatenate images channel-wise
+    # concatenate images channel-wise
     merged = Concatenate()([in_src_image, in_target_image])
-	# C64
+    # C64
     d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
     d = LeakyReLU(alpha=0.2)(d)
-	# C128
+    # C128
     d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
     d = BatchNormalization()(d)
     d = LeakyReLU(alpha=0.2)(d)
-	# C256
+    # C256
     d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
     d = BatchNormalization()(d)
     d = LeakyReLU(alpha=0.2)(d)
-	# C512
+    # C512
     d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
     d = BatchNormalization()(d)
     d = LeakyReLU(alpha=0.2)(d)
-	# second last output layer
+    # second last output layer
     d = Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
     d = BatchNormalization()(d)
     d = LeakyReLU(alpha=0.2)(d)
-	# patch output
+    # patch output
     d = Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
     patch_out = Activation('sigmoid')(d)
-	# define model
+    # define model
     model = Model([in_src_image, in_target_image], patch_out)
-	# compile model
+    # compile model
     opt = Adam(lr=0.0002, beta_1=0.5)
     model.compile(loss='binary_crossentropy', optimizer=opt, loss_weights=[0.5])
 
@@ -61,14 +61,14 @@ def define_encoder_block(layer_in, n_filters, batchnorm=True):
     Here, we define the encoder block.
     """
 
-	# weight initialization
+    # weight initialization
     init = RandomNormal(stddev=0.02)
-	# add downsampling layer
+    # add downsampling layer
     g = Conv2D(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
-	# conditionally add batch normalization
+    # conditionally add batch normalization
     if batchnorm:
         g = BatchNormalization()(g, training=True)
-	# leaky relu activation
+    # leaky relu activation
     g = LeakyReLU(alpha=0.2)(g)
 
     return g
@@ -79,18 +79,18 @@ def decoder_block(layer_in, skip_in, n_filters, dropout=True):
     Here, we define the decoder block.
     """
 
-	# weight initialization
+    # weight initialization
     init = RandomNormal(stddev=0.02)
-	# add upsampling layer
+    # add upsampling layer
     g = Conv2DTranspose(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
-	# add batch normalization
+    # add batch normalization
     g = BatchNormalization()(g, training=True)
-	# conditionally add dropout
+    # conditionally add dropout
     if dropout:
         g = Dropout(0.5)(g, training=True)
-	# merge with skip connection
+    # merge with skip connection
     g = Concatenate()([g, skip_in])
-	# relu activation
+    # relu activation
     g = Activation('relu')(g)
 
     return g
@@ -102,11 +102,11 @@ def define_generator(image_shape=(256, 256, 1)):
     It may vary the architecture as a function of the passed input shape.
     """
 
-	# weight initialization
+    # weight initialization
     init = RandomNormal(stddev=0.02)
-	# image input
+    # image input
     in_image = Input(shape=image_shape)
-	# encoder model
+    # encoder model
     e1 = define_encoder_block(in_image, 64, batchnorm=False)
     e2 = define_encoder_block(e1, 128)
     e3 = define_encoder_block(e2, 256)
@@ -114,10 +114,10 @@ def define_generator(image_shape=(256, 256, 1)):
     e5 = define_encoder_block(e4, 512)
     e6 = define_encoder_block(e5, 512)
     e7 = define_encoder_block(e6, 512)
-	# bottleneck, no batch norm and relu
+    # bottleneck, no batch norm and relu
     b = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(e7)
     b = Activation('relu')(b)
-	# decoder model
+    # decoder model
     d1 = decoder_block(b, e7, 512)
     d2 = decoder_block(d1, e6, 512)
     d3 = decoder_block(d2, e5, 512)
@@ -126,10 +126,10 @@ def define_generator(image_shape=(256, 256, 1)):
     d6 = decoder_block(d5, e2, 128, dropout=False)
     d7 = decoder_block(d6, e1, 64, dropout=False)
     
-	# Changed the 1 below from 3 (gray ipv RGB)
+    # Changed the 1 below from 3 (gray ipv RGB)
     g = Conv2DTranspose(1, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d7)
     out_image = Activation('tanh')(g)
-	# define model
+    # define model
     model = Model(in_image, out_image)
 
     return model
@@ -141,21 +141,21 @@ def define_gan(g_model, d_model, image_shape):
     We also pass the input shape.
     """
 
-	# make weights in the discriminator not trainable
+    # make weights in the discriminator not trainable
     for layer in d_model.layers:
         if not isinstance(layer, BatchNormalization):
             layer.trainable = False
-	# define the source image
+    # define the source image
     in_src = Input(shape=image_shape)
-	# connect the source image to the generator input
+    # connect the source image to the generator input
     gen_out = g_model(in_src)
-	# connect the source input and generator output to the discriminator input
+    # connect the source input and generator output to the discriminator input
     dis_out = d_model([in_src, gen_out])
-	# src image as input, generated image and classification output
+    # src image as input, generated image and classification output
         
     model = Model(in_src, [dis_out, gen_out])
 
-	# compile model
+    # compile model
     opt = Adam(lr=0.0002, beta_1=0.5)
     model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1,100])
     return model
