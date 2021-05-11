d8cfba94c7dbe08441d554bcf581a7c302e61866
shaverschuren <s.h.a.verschuren@student.tue.nl>
2021-05-11 15:32:58 +0200

    Added plot function

---
+++ src/plots.py
@@ -0,0 +1,93 @@
+import sys
+if "" not in sys.path: sys.path.append("")
+
+import os
+from glob import glob
+import warnings
+import numpy as np
+from PIL import Image, ImageFilter
+from util.general import *
+
+
+def drawContour(m, s, RGB):
+    """
+    Draw edges of contour from segmented image 's' onto 'm' in colour
+    'RGB'
+    """
+    # Fill contour "c" with white, make all else black
+    thisContour = s.point(lambda p: p > 1e-5 and 255)
+    # DEBUG: thisContour.save(f"interim{c}.png")
+
+    # Find edges of this contour and make into Numpy array
+    thisEdges = thisContour.filter(ImageFilter.FIND_EDGES)
+    thisEdgesN = np.array(thisEdges)
+
+    # Paint locations of found edges in color "RGB" onto "main"
+    m[np.nonzero(thisEdgesN)] = RGB
+    return m
+
+
+def drawFill(m, s, RGB):
+    """
+    Draw fill from segmented image 's' onto 'm' in colour 'RGB'
+    """
+    # Fill contour "c" with white, make all else black
+    thisContour = s.point(lambda p: p > 1e-5 and 255)
+    thisContourN = np.array(thisContour)
+
+    # Find color addition
+    addition = [0, 0, 0]
+    for i in range(3):
+        addition[i] = 0.5 * RGB[i]
+
+    additionT = tuple(addition)
+
+    # Paint locations of found edges in color "RGB" onto "main"
+    m[np.nonzero(thisContourN)] = m[np.nonzero(thisContourN)] * 0.5 + additionT
+    return m
+
+
+def create_subject_plot(src_path, pred_path, tar_path, out_path):
+    """
+    This function creates overlay plots used for the
+    paper. It simply takes the original image, overlays
+    the prediction and target masks and then stores the
+    image as a png.
+    """
+
+    src_img = Image.open(src_path).convert('L').convert('RGB')
+    tar_img = Image.open(tar_path).convert('L')
+    pred_img = Image.open(pred_path).convert('L')
+
+    src_img.resize((2048, 2048), resample=Image.BILINEAR)
+    tar_img.resize((2048, 2048), resample=Image.BILINEAR)
+    pred_img.resize((2048, 2048), resample=Image.BILINEAR)
+
+    main = np.array(src_img)
+
+    main = drawFill(main, pred_img, (180, 0, 0))
+    main = drawContour(main, tar_img, (0, 0, 255))
+
+    Image.fromarray(main).save(out_path)
+
+
+def plot_subjects(testDir, subject_list):
+    """
+    This function creates plots for a list of subjects.
+    """
+
+    if not os.path.isdir(os.path.join(testDir, "plots")):
+        os.mkdir(os.path.join(testDir, "plots"))
+
+    for subject in subject_list:
+        src_path = os.path.join(testDir, "src", subject + ".png")
+        tar_path = os.path.join(testDir, "tar", subject + ".png")
+        pred_path = os.path.join(testDir, "pred", subject + ".png")
+        out_path = os.path.join(testDir, "plots", subject + ".png")
+
+        create_subject_plot(src_path, pred_path, tar_path, out_path)
+
+
+if __name__ == "__main__":
+    plot_subjects(os.path.join("data", "test"),
+                  ["0_005", "0_071", "0_073"])
